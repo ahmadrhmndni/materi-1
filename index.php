a9210a0b6318ce90308a2bb97eb00a1705dc19a1
ahmadrhmndni <ahmadaksen123@gmail.com>
2020-09-12 16:02:48 +0700

    polymorphism

--- index.php
+++ index.php
@@ -1,24 +1,32 @@
 <?php  
 
-class Mobil {
-	private $merk, $tipe, $mesin, $max_speed;
+class Mobil{
+	public $merk, $tipe, $mesin, $max_speed;
 
 	public function cetakTipe(){
 		return $this->tipe;
-		}
+	}
 	public function kecepatanMaksimal(){
 			return "Kecepatan Maksimal Dari Mobil Ini Adalah".$this->max_speed;
-		}
+	}
+		 function injakgas(){
+			return "Mengalirkan bensin keruang bakar, rpm naik, roda berputar";
+	}
+	function injekgas(){
+		return "Mengalirkan Pertalite keruang bakar, rpm naik, roda berputar";
+	}
 }
 
-class BMW extends Mobil {
+class BMW extends Mobil{
 
 }
 
-class Dacia extends Mobil{
+class dacia extends Mobil{
 	function selfParking(){
 		echo "Parkir Sendiri";
 	}
+
+	
 }
 
 $bmw = new Mobil;
@@ -27,13 +35,15 @@ $bmw->tipe = "320i";
 $bmw->mesin = "2000cc";
 $bmw->max_speed = "280 km/h";
 
-$Dacia = new Mobil;
-$Dacia->merk = "Daihatsu";
-$Dacia->tipe = "Model-A";
-$Dacia->mesin = "1500cc";
-$Dacia->max_speed = "200 km/h";
+$dacia = new Mobil;
+$dacia->merk = "Daihatsu";
+$dacia->tipe = "Model-A";
+$dacia->mesin = "1500cc";
+$dacia->max_speed = "200 km/h";
 
-echo $Dacia->kecepatanMaksimal();
+echo "BMW : ".$bmw->injakgas();
+echo"<br>";
+echo "DACIA : ".$dacia->injekgas();
 
 
 
